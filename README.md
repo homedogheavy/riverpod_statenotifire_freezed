a1576d1dae82aa6f5c62bf80812419deb178bd31
kyu-suke <ykaneoka7@gmail.com>
2021-05-24 21:37:22 +0900

    sample

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # riverpod_statenotifire_freezed
-flutter riverpod statenotifier freezed
+## freezed
+flutter pub run build_runner build --delete-conflicting-outputs
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_statenotifire_freezed/counter_state.dart';
+
+final counterProvider =
+StateNotifierProvider<CounterStateNotifier, CounterState>(
+        (ref) => CounterStateNotifier());
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('state_notifier sample'),
+      ),
+      body: Center(
+        child: Consumer(builder: (context, watch, _) {
+          final count = watch(counterProvider);
+          print(count);
+          return Text('$count');
+        }),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => context.read(counterProvider.notifier).increment(),
+        label: Text('1'),
+        icon: Icon(Icons.add),
+      ),
+    );
+  }
+}
